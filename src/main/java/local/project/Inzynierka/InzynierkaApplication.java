6e670d5c9718ec41628b038e08ba73bf348df1ec
mmateu <45851518+mmateu@users.noreply.github.com>
2019-04-06 15:39:10 +0200

    Create Spring project

---
+++ src/main/java/local/project/Inzynierka/InzynierkaApplication.java
@@ -0,0 +1,13 @@
+package local.project.Inzynierka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InzynierkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InzynierkaApplication.class, args);
+	}
+
+}

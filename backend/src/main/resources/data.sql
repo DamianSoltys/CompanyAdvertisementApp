d98a06f2fba1eac6c472747bc0b5e41589407665
mmateu <45851518+mmateu@users.noreply.github.com>
2019-05-15 20:16:33 +0200

    Second part of previous commit lol

---
+++ backend/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO VOIVODESHIPS(name) VALUES ('dolnośląskie'),('kujawsko-pomorskie'),('lubelskie'), ('lubuskie'),
+('łódzkie'), ('małopolskie'), ('mazowieckie'), ('opolskie'), ('podkarpackie'), ('podlaskie'), ('pomorskie'), ('śląskie'),
+('świętokrzyskie'), ('warmińsko-mazurskie'), ('wielkopolskie'),('zachodniopomorskie');

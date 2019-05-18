13d220fdfa2001edd3ba35ccfd22af07c66b509b
DamianSoltys <soltysiukd@gmail.com>
2019-05-18 17:21:39 +0200

    Added validation(name,email) to register form

--- frontend/src/app/register/register.component.ts
+++ frontend/src/app/register/register.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, FormControl } from '@angular/forms';
+import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
 import { UserReg } from '../classes/User';
 import { RegisterService } from '../services/register.service';
 
@@ -15,11 +15,24 @@ export class RegisterComponent implements OnInit {
 
   ngOnInit() {
     this.registerForm = this.fb.group({
-      name: [''],
-      email: [''],
-      password: [''],
-      checkpassword: ['']
+      name: ['',
+      [Validators.required, Validators.pattern(new RegExp(/^[A-Za-zżźćńółęąśŻŹĆĄŚĘŁÓŃ]+$/)),
+      Validators.minLength(4)]],
+
+      email: ['',
+      // tslint:disable-next-line:max-line-length
+      [Validators.required, Validators.email]],
+      password: ['',
+      [Validators.required, Validators.pattern(new RegExp(/^(?=.{1,})(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?!.*\s).*$/))],
+      Validators.minLength(8)],
+      checkpassword: ['',
+      [Validators.required, Validators.pattern(new RegExp(/^(?=.{1,})(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?!.*\s).*$/))],
+    Validators.minLength(8)]
     });
+  }
+
+  get regForm(){
+    return this.registerForm.controls;
   }
  onSubmit() {
     const User_Data = new UserReg();
@@ -31,5 +44,8 @@ export class RegisterComponent implements OnInit {
    }, (error) => {
      console.log(error);
    });
+ }
+ onSubmitt(){
+   console.log("test");
  }
 }

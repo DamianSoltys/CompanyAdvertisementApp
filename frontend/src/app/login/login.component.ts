1a3b68d638fe10c8c0a8e2f14ddc796851a2e250
DamianSoltys <soltysiukd@gmail.com>
2019-05-18 17:52:25 +0200

    Added validation divs to html file in login form

--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -19,6 +19,9 @@ export class LoginComponent implements OnInit {
       password: ['']
     });
   }
+get logform(){
+  return this.loginForm.controls;
+}
   onSubmit() {
     const User_data = new UserLog();
     User_data.email = this.loginForm.get('email').value;

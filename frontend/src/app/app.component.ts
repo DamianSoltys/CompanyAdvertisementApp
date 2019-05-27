dec1e611d7b43490df86dd6699554d371eb9e6d0
DamianSoltys <soltysiukd@gmail.com>
2019-05-27 03:11:04 +0200

    Updated animations on menu and nearby with Angular animations module

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,27 +1,68 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterContentChecked } from '@angular/core';
+import { trigger, state, style, animate, transition } from '@angular/animations';
+import { BehaviorSubject, Observable } from 'rxjs';
 
-import * as $ from 'jquery';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations: [
+    trigger('showHide', [
+      state('visible', style({
+        opacity: '1',
+        display: 'block'
+
+      })),
+      state('hidden', style({
+        opacity: '0',
+        display: 'none'
+
+      })),
+      transition('visible <=> hidden', [
+        animate('1s')
+      ]),
+
+    ]),
+    trigger('showHideMenu', [
+      state('visible', style({
+      opacity: '1',
+      display: 'block'
+
+
+      })),
+      state('hidden', style({
+        opacity: '0',
+        display: 'none'
+
+
+      })),
+      transition('visible <=> hidden', [
+        animate('0.5s')
+      ]),
+
+    ]),
+  ],
 })
 export class AppComponent implements OnInit {
   nearby_toggle = false;
   logged = false;
-  ngOnInit(): void {}
+  visible = true;
+  displayMenu = new BehaviorSubject(false);
+  constructor() {
+    document.body.addEventListener('click', (e) => {
+     if ((<HTMLElement>e.target).id !== 'menuId') {
+       this.displayMenu.next(false);
+     }
 
-  menuAnimation() {
-    $('.dropdown-menu').slideToggle('slow', () => {
-      $(document).ready(() => {
-        $(document).on('click', () => {
-          $('.dropdown-menu').slideUp('slow', () => {});
-        });
-      });
     });
   }
-  toggleNearby() {
+  ngOnInit(): void {}
+
+  toggleNearbyBlock() {
     this.nearby_toggle = !this.nearby_toggle;
-    $('.nearby').slideToggle('slow');
+    this.visible = !this.visible;
+  }
+  toggleMenu() {
+    this.displayMenu.next(!this.displayMenu.value);
   }
 }
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -16,7 +17,8 @@ import { HttpClientModule } from '@angular/common/http';
     BrowserModule,
     AppRoutingModule,
     ReactiveFormsModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule
 
 
   ],

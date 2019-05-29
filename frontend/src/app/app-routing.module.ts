427c96046a66136264bd7bff4d7f233653e229a3
DamianSoltys <soltysiukd@gmail.com>
2019-05-29 23:01:20 +0200

    Added personalData component and authGuard ,also routes were updated

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AuthGuard } from './guards/auth-guard.service';
 
 const routes: Routes = [
   {path: '', redirectTo: 'home', pathMatch: 'full'},
@@ -11,7 +12,12 @@ const routes: Routes = [
     path: 'register', loadChildren: './register/register.module#RegisterModule'
   },
   {
-    path:'search',loadChildren: './search/search.module#SearchModule'
+    path: 'search', loadChildren: './search/search.module#SearchModule'
+  },
+  {
+    path: 'user/personalData',
+    loadChildren: './user/personal-data/personal-data.module#PersonalDataModule',
+    canActivate: [AuthGuard]
   }
 ];
 
---
+++ frontend/src/app/guards/auth-guard.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router, Route } from '@angular/router';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+
+  constructor(private router: Router) { }
+
+    canActivate(
+      route: ActivatedRouteSnapshot,
+      state: RouterStateSnapshot):
+      Observable<boolean> | Promise<boolean> | boolean {
+        console.log('elo');
+      return true;
+    }
+  }
+
+

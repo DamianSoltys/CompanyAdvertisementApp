920969c14c3c14a9082fe1e2e6d75501b1810cd2
mmateu <45851518+mmateu@users.noreply.github.com>
2019-08-08 21:02:13 +0200

    #30 Make inserts to db reusable

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -1,8 +1,20 @@
-INSERT INTO voivodeships(name) VALUES ('dolnośląskie'),('kujawsko-pomorskie'),('lubelskie'), ('lubuskie'),
+DELIMITER |
+CREATE OR REPLACE PROCEDURE insert_voivoideships()
+BEGIN
+
+INSERT IGNORE INTO voivodeships(name) VALUES ('dolnośląskie'),('kujawsko-pomorskie'),('lubelskie'), ('lubuskie'),
 ('łódzkie'), ('małopolskie'), ('mazowieckie'), ('opolskie'), ('podkarpackie'), ('podlaskie'), ('pomorskie'), ('śląskie'),
 ('świętokrzyskie'), ('warmińsko-mazurskie'), ('wielkopolskie'),('zachodniopomorskie');
+END |
 
-INSERT INTO email_addresses(email_id, email, created_at) VALUES
+DELIMITER ;
+CALL inzynierka.insert_voivoideships();
+
+
+DELIMITER |
+CREATE OR REPLACE PROCEDURE insert_emails()
+BEGIN
+INSERT IGNORE INTO email_addresses(email_id, email, created_at) VALUES
 (1, 'example@example.com', NOW()),
 (2, 'example2@example.com', NOW()),
 (3, 'example3@example.com', NOW()),
@@ -11,16 +23,29 @@ INSERT INTO email_addresses(email_id, email, created_at) VALUES
 (6, 'example6@example.com', NOW()),
 (7, 'example7@example.com', NOW()),
 (8, 'example8@example.com', NOW());
+END |
+DELIMITER ;
 
-INSERT INTO addresses(apartment_no, building_no, city, created_at, modified_at, street, voivodeship_id)  VALUES
+CALL insert_emails();
+
+DELIMITER |
+CREATE OR REPLACE PROCEDURE insert_addresses()
+BEGIN
+INSERT IGNORE INTO addresses(apartment_no, building_no, city, created_at, modified_at, street, voivodeship_id)  VALUES
 ('5', '43','Kraków', NOW(), NOW(), 'Swojska', 1),
 ('2', '23','Lublin', NOW(), NOW(), 'Wasza', 2),
 ('5', '23','Szczebrzeszyn', NOW(), NOW(), 'Koziołka Matołka', 3),
 ('5', '4','Kraków', NOW(), NOW(), 'Makówkowa', 4),
 ('5', '2','Warszawa', NOW(), NOW(), 'Ichniejsza', 5);
+END |
+DELIMITER ;
 
+CALL insert_addresses();
 
-INSERT INTO natural_persons(id_natural_person, first_name, last_name, phone_no, created_at, modified_at, address_id) VALUES
+DELIMITER |
+CREATE OR REPLACE PROCEDURE insert_natural_persons()
+BEGIN
+INSERT IGNORE INTO natural_persons(id_natural_person, first_name, last_name, phone_no, created_at, modified_at, address_id) VALUES
 (1, 'Karol', 'Karolak', '555111222', NOW(), NOW(), 1 ),
 (2, 'Maciej', 'Kozidrak', '535411222', NOW(), NOW(), 2),
 (3, 'Agata', 'Trzeciak', '355111222', NOW(), NOW(), 3 ),
@@ -30,38 +55,75 @@ INSERT INTO natural_persons(id_natural_person, first_name, last_name, phone_no, 
 (7, 'Konrad', 'Pęczarski', '555111111', NOW(), NOW(),2),
 (8, 'Karol', 'Maciejak', '555222222', NOW(), NOW(),3),
 (9, 'Marcin', 'Lewandowski', '111111222', NOW(), NOW(),4);
+END |
 
+CALL insert_natural_persons();
 
+DELIMITER |
+
+CREATE OR REPLACE PROCEDURE insert_users()
+BEGIN
 # --password - Haslo1
-INSERT INTO users( user_name, password_hash, modified_at,created_at, id_natural_person, id_email_address, account_type, is_enabled) VALUES
+INSERT IGNORE INTO users( user_name, password_hash, modified_at,created_at, id_natural_person, id_email_address, account_type, is_enabled) VALUES
 ( 'biedronka', '$2a$10$lb/zKrT4Pey1JCYdGbWKn.Nn61spi./CT/rSdoSOKO/ChBoaLHhtu', NOW(), NOW(), 3, 1, 0, true ),
 ( 'kurczak5', '$2a$10$lb/zKrT4Pey1JCYdGbWKn.Nn61spi./CT/rSdoSOKO/ChBoaLHhtu', NOW(), NOW(), NULL, 2, 0 , true),
 ( 'magicznyKrzystof', '$2a$10$lb/zKrT4Pey1JCYdGbWKn.Nn61spi./CT/rSdoSOKO/ChBoaLHhtu', NOW(), NOW(), NULL, 3, 0, false),
 ( 'Gacek', '$2a$10$lb/zKrT4Pey1JCYdGbWKn.Nn61spi./CT/rSdoSOKO/ChBoaLHhtu', NOW(), NOW(), NULL, 4, 0, false ),
 ( 'maciejowicz', '$2a$10$lb/zKrT4Pey1JCYdGbWKn.Nn61spi./CT/rSdoSOKO/ChBoaLHhtu', NOW(), NOW(), NULL, 5, 0, false ),
 ( 'jezyk', '$2a$10$lb/zKrT4Pey1JCYdGbWKn.Nn61spi./CT/rSdoSOKO/ChBoaLHhtu', NOW(), NOW(), 5, 6, 0, true );
+END |
 
-INSERT INTO categories(id, name, created_at, modified_at) VALUES
+CALL insert_users();
+
+DELIMITER |
+
+CREATE OR REPLACE PROCEDURE insert_categories()
+BEGIN
+INSERT IGNORE INTO categories(id, name, created_at, modified_at) VALUES
 (1, 'Motoryzacja', NOW(), NOW()),
 (2, 'Technologia', NOW(), NOW()),
 (3, 'Usługi', NOW(), NOW()),
 (4, 'Fotografia', NOW(), NOW()),
 (5, 'Restauracje', NOW(), NOW()),
 (6, 'Uroda', NOW(), NOW());
+END |
 
-INSERT INTO
+CALL insert_categories();
+
+DELIMITER |
+
+CREATE OR REPLACE PROCEDURE insert_companies()
+BEGIN
+INSERT IGNORE INTO
 companies(id, name, NIP, REGON,  description, registerer_id, category_id, created_at, modified_at, has_branch, address_id)
 VALUES
 (1, 'Mechanikex', '1234567890', '12345678901234', 'Najlepszy w mieście mechanik.', 2, 1, NOW(), NOW(), TRUE, 1),
 (2, 'Urodex', '2234567890', '22345678901234', 'Najlepszy w mieście zakład fryzjerski.', 3, 6, NOW(), NOW(), TRUE, 3);
+END |
+DELIMITER ;
 
-INSERT INTO
+CALL insert_companies();
+
+DELIMITER  |
+CREATE OR REPLACE PROCEDURE insert_branches()
+BEGIN
+INSERT IGNORE INTO
 branches(branch_id, registerer_id, company_id, name, created_at, modified_at, x_geo_coordinate, y_geo_coordinate, address_id) VALUES
 (1, 2, 1, 'Mechanikex', NOW(), NOW(), 51.1079, 17.0385, 1),
 (2, 2, 1, 'Mechanikex', NOW(), NOW(), 51.1071, 17.0383, 2),
 (3, 3, 2, 'Urodex', NOW(), NOW(), 51.2167, 22.734,3 );
+END |
+DELIMITER ;
 
-INSERT INTO newsletter_subscriptions(id, company_id,id_email, verified, created_at, modified_at) VALUES
+CALL insert_branches();
+
+DELIMITER |
+
+CREATE OR REPLACE PROCEDURE insert_newsletter_subscriptions()
+BEGIN
+INSERT IGNORE INTO newsletter_subscriptions(id, company_id,id_email, verified, created_at, modified_at) VALUES
 (1, 1,7, FALSE, NOW(), NOW()),
 (2, 1,8, FALSE, NOW(), NOW());
-
+END |
+DELIMITER ;
+CALL insert_newsletter_subscriptions();

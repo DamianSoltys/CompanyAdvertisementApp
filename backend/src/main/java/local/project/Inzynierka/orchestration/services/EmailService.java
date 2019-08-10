d7b989d741bfe67c3067eb3eb08ef94b5ea3d671
mmateu <45851518+mmateu@users.noreply.github.com>
2019-08-10 14:32:02 +0200

    #32 Remove unneeded service interfaces
    *Remove unused code

--- backend/src/main/java/local/project/Inzynierka/orchestration/services/EmailService.java
+++ backend/src/main/java/local/project/Inzynierka/orchestration/services/EmailService.java
@@ -1,10 +1,29 @@
 package local.project.Inzynierka.orchestration.services;
 
 import local.project.Inzynierka.persistence.entity.EmailAddress;
+import local.project.Inzynierka.persistence.repository.EmailRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
-public interface EmailService {
+@Service
+public class EmailService {
 
-    EmailAddress findByEmail(EmailAddress email);
+    @Autowired
+    private EmailRepository emailRepository;
 
-    EmailAddress saveEmailAddress(EmailAddress emailAddress);
+    public EmailAddress findByEmail(EmailAddress email) {
+
+        EmailAddress emailAddressEntity = emailRepository.findByEmail(email.getEmail());
+        if (emailAddressEntity == null) {
+            return null;
+        }
+        return emailAddressEntity;
+    }
+
+    public EmailAddress saveEmailAddress(EmailAddress emailAddress) {
+
+        EmailAddress emailAddressEntity = emailRepository.save(emailAddress);
+
+        return emailAddressEntity;
+    }
 }
--- backend/src/main/java/local/project/Inzynierka/orchestration/services/UserService.java
+++ backend/src/main/java/local/project/Inzynierka/orchestration/services/UserService.java
@@ -1,20 +1,117 @@
 package local.project.Inzynierka.orchestration.services;
 
+import local.project.Inzynierka.persistence.entity.Address;
 import local.project.Inzynierka.persistence.entity.EmailAddress;
 import local.project.Inzynierka.persistence.entity.NaturalPerson;
 import local.project.Inzynierka.persistence.entity.User;
+import local.project.Inzynierka.persistence.entity.VerificationToken;
+import local.project.Inzynierka.persistence.entity.Voivoideship;
+import local.project.Inzynierka.persistence.repository.AddressRepository;
+import local.project.Inzynierka.persistence.repository.NaturalPersonRepository;
+import local.project.Inzynierka.persistence.repository.UserRepository;
+import local.project.Inzynierka.persistence.repository.VerificationTokenRepository;
+import local.project.Inzynierka.persistence.repository.VoivodeshipRepository;
+import local.project.Inzynierka.shared.AuthenticationFacade;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-public interface UserService {
+@Service
+@Slf4j
+public class UserService {
 
-    User findByName(String name);
+    @Autowired
+    private UserRepository userRepository;
 
-    User findByEmailAddress(EmailAddress emailAddress);
+    @Autowired
+    private VerificationTokenRepository verificationTokenRepository;
 
-    User createNewUser(User user);
+    @Autowired
+    private VoivodeshipRepository voivodeshipRepository;
 
-    void createVerificationTokenForUser(User user, final String token);
+    @Autowired
+    private NaturalPersonRepository naturalPersonRepository;
 
-    boolean verifyToken(String token);
+    @Autowired
+    private AuthenticationFacade authenticationFacade;
 
-    boolean becomeNaturalPerson(NaturalPerson naturalPerson);
+    @Autowired
+    private AddressRepository addressRepository;
+
+    public User findByName(String name) {
+
+        User user = userRepository.findByName(name);
+        return user;
+    }
+
+    public User findByEmailAddress(EmailAddress emailAddress) {
+        User user = userRepository.getByAddressEmail(emailAddress.getEmail());
+
+        return user;
+    }
+
+    public User createNewUser(User user) {
+
+        user.setAccountType((short) 0);
+
+        return userRepository.save(user);
+
+    }
+
+    @Transactional
+    public void createVerificationTokenForUser(User user, final String token) {
+        VerificationToken myToken = new VerificationToken(token);
+        myToken.setId(0L);
+
+        VerificationToken createdToken = verificationTokenRepository.save(myToken);
+        user.setVerificationToken(createdToken);
+        userRepository.save(user);
+    }
+
+    @Transactional
+    public boolean verifyToken(String token) {
+        VerificationToken verificationToken = verificationTokenRepository.findByToken(token);
+        if (verificationToken == null) {
+            return false;
+        }
+
+        User user = userRepository.findByVerificationToken(verificationToken);
+        user.setEnabled(true);
+
+        userRepository.save(user);
+        return true;
+    }
+
+    @Transactional
+    public boolean becomeNaturalPerson(NaturalPerson naturalPerson) {
+
+        naturalPerson.setId(0L);
+
+        Voivoideship voivoideship = voivodeshipRepository.findByName(naturalPerson.getAddress().getVoivodeship_id().getName());
+        if (voivoideship == null) {
+            return false;
+        }
+
+        Address address = Address.builder()
+                .apartmentNo(naturalPerson.getAddress().getApartmentNo())
+                .buildingNo(naturalPerson.getAddress().getApartmentNo())
+                .id(0L)
+                .city(naturalPerson.getAddress().getCity())
+                .voivodeship_id(voivoideship)
+                .street(naturalPerson.getAddress().getStreet())
+                .build();
+
+        address = addressRepository.save(address);
+        naturalPerson.setAddress(address);
+
+        naturalPerson = naturalPersonRepository.save(naturalPerson);
+        System.out.println(authenticationFacade.getAuthentication().getName());
+        User user = userRepository.getByAddressEmail(authenticationFacade.getAuthentication().getName());
+
+        user.setNaturalPerson(naturalPerson);
+        userRepository.save(user);
+
+        return true;
+    }
 }

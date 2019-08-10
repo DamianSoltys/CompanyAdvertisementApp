e7ca48ad0584e1fec5d87c90561dd60e465dba16
mmateu <45851518+mmateu@users.noreply.github.com>
2019-08-10 18:10:08 +0200

    #32 Clean up EmailService

--- backend/src/main/java/local/project/Inzynierka/orchestration/services/EmailService.java
+++ backend/src/main/java/local/project/Inzynierka/orchestration/services/EmailService.java
@@ -2,28 +2,24 @@ package local.project.Inzynierka.orchestration.services;
 
 import local.project.Inzynierka.persistence.entity.EmailAddress;
 import local.project.Inzynierka.persistence.repository.EmailRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class EmailService {
 
-    @Autowired
-    private EmailRepository emailRepository;
+    private final EmailRepository emailRepository;
+
+    public EmailService(EmailRepository emailRepository) {
+        this.emailRepository = emailRepository;
+    }
 
     public EmailAddress findByEmail(EmailAddress email) {
 
-        EmailAddress emailAddressEntity = emailRepository.findByEmail(email.getEmail());
-        if (emailAddressEntity == null) {
-            return null;
-        }
-        return emailAddressEntity;
+        return emailRepository.findByEmail(email.getEmail());
     }
 
     public EmailAddress saveEmailAddress(EmailAddress emailAddress) {
 
-        EmailAddress emailAddressEntity = emailRepository.save(emailAddress);
-
-        return emailAddressEntity;
+        return emailRepository.save(emailAddress);
     }
 }

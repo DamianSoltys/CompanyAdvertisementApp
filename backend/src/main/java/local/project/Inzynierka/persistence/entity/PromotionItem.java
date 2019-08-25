60dc4ab7f538250eee00c56893cc9449eda249b1
mmateu <45851518+mmateu@users.noreply.github.com>
2019-08-25 13:10:58 +0200

    #54 Rename column and constraints

--- backend/src/main/java/local/project/Inzynierka/persistence/entity/PromotionItem.java
+++ backend/src/main/java/local/project/Inzynierka/persistence/entity/PromotionItem.java
@@ -38,8 +38,8 @@ public class PromotionItem extends FullTimestampingAudit implements IEntity<Long
     @JoinColumn(name = "promotion_type_id", nullable = false,  foreignKey = @ForeignKey(name = "promotion_type_FK"))
     private PromotionItemType promotionItemType;
 
-    @Column(name = "valid")
-    private boolean valid;
+    @Column(name = "was_sent")
+    private boolean wasSent;
 
     @Column(nullable = false, name = "valid_from", columnDefinition = "TIMESTAMP")
     private Timestamp validFrom;

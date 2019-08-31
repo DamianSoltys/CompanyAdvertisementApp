bd1f6a7f6420611547106203b6d3b906ede528d1
mmateu <45851518+mmateu@users.noreply.github.com>
2019-08-31 22:02:32 +0200

    #66 Remove UserRepository from AccessPermissionService

--- backend/src/main/java/local/project/Inzynierka/web/security/AccessPermissionService.java
+++ backend/src/main/java/local/project/Inzynierka/web/security/AccessPermissionService.java
@@ -1,32 +1,30 @@
 package local.project.Inzynierka.web.security;
 
 import local.project.Inzynierka.persistence.entity.User;
-import local.project.Inzynierka.persistence.repository.UserRepository;
 import local.project.Inzynierka.shared.AuthenticationFacade;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AccessPermissionService {
 
-    private final UserRepository userRepository;
-
     private final AuthenticationFacade authenticationFacade;
 
-    public AccessPermissionService(UserRepository userRepository, AuthenticationFacade authenticationFacade) {
-        this.userRepository = userRepository;
+    public AccessPermissionService(AuthenticationFacade authenticationFacade) {
         this.authenticationFacade = authenticationFacade;
     }
 
     public boolean hasPrincipalHavePermissionToUserResource(Long userId) {
-        User authenticatedUser = this.userRepository.getByAddressEmail(authenticationFacade.getAuthentication().getName());
-        User requestedUser = this.userRepository.findById(userId).orElse(new User());
+        User authenticatedUser = this.authenticationFacade.getAuthenticatedUser();
 
-        return authenticatedUser != null && authenticatedUser.equals(requestedUser);
+        return authenticatedUser != null && authenticatedUser.getId().equals(userId);
     }
 
     public boolean hasPrincipalHavePermissionToNaturalPersonResource(Long userId, Long personId) {
-        User authenticatedUser = this.userRepository.getByAddressEmail(authenticationFacade.getAuthentication().getName());
-        return authenticatedUser.hasRegisteredNaturalPerson() &&
+        User authenticatedUser = this.authenticationFacade.getAuthenticatedUser();
+
+        return authenticatedUser != null &&
+                authenticatedUser.getId().equals(userId) &&
+                authenticatedUser.hasRegisteredNaturalPerson() &&
                 authenticatedUser.getNaturalPerson().getId().equals(personId);
     }
 }

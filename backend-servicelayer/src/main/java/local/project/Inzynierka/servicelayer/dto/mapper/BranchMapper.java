2578086c75e7ad5d412c48089cd2b6f849dcd276
mmateu <45851518+mmateu@users.noreply.github.com>
2019-10-08 15:36:58 +0200

    #95 Add branchIds to getAll branches endpoint

--- backend-servicelayer/src/main/java/local/project/Inzynierka/servicelayer/dto/mapper/BranchMapper.java
+++ backend-servicelayer/src/main/java/local/project/Inzynierka/servicelayer/dto/mapper/BranchMapper.java
@@ -2,6 +2,7 @@ package local.project.Inzynierka.servicelayer.dto.mapper;
 
 import local.project.Inzynierka.persistence.entity.Branch;
 import local.project.Inzynierka.servicelayer.dto.CompanyBranchDto;
+import local.project.Inzynierka.servicelayer.dto.PersistedBranchDto;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -10,7 +11,7 @@ import java.util.stream.Collectors;
 @Component
 public class BranchMapper {
 
-    public CompanyBranchDto map(Branch branch) {
+    public CompanyBranchDto mapInputBranch(Branch branch) {
         CompanyBranchDto companyBranchDto = new CompanyBranchDto();
         companyBranchDto.setAddress(new AddressMapper().map(branch.getAddress()));
         companyBranchDto.setGeoX(branch.getGeoX());
@@ -19,9 +20,25 @@ public class BranchMapper {
         return companyBranchDto;
     }
 
-    public List<CompanyBranchDto> map(List<Branch> branches) {
+    public List<CompanyBranchDto> mapInputBranch(List<Branch> branches) {
         return branches.stream()
-                .map(this::map)
+                .map(this::mapInputBranch)
+                .collect(Collectors.toList());
+    }
+
+    public PersistedBranchDto mapPersistedBranch(Branch branch) {
+        PersistedBranchDto persistedBranchDto = new PersistedBranchDto();
+        persistedBranchDto.setAddress(new AddressMapper().map(branch.getAddress()));
+        persistedBranchDto.setGeoX(branch.getGeoX());
+        persistedBranchDto.setGeoY(branch.getGeoY());
+        persistedBranchDto.setName(branch.getName());
+        persistedBranchDto.setBranchId(branch.getId());
+        return persistedBranchDto;
+    }
+
+    public List<PersistedBranchDto> mapPersistedBranch(List<Branch> branches) {
+        return branches.stream()
+                .map(this::mapPersistedBranch)
                 .collect(Collectors.toList());
     }
 }

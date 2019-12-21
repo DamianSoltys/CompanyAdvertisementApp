036adfe15a03a650d233768efa0b69319a435883
DamianSoltys <soltysiukd@gmail.com>
2019-12-22 00:39:21 +0100

    Change to auth guard

--- frontend/src/app/guards/auth-guard.service.ts
+++ frontend/src/app/guards/auth-guard.service.ts
@@ -1,42 +1,39 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
 import {
   CanActivate,
   ActivatedRouteSnapshot,
   RouterStateSnapshot,
   Router,
   Route
-} from '@angular/router';
-import { Observable } from 'rxjs';
-import { LoginService } from '../services/login.service';
-import { UserService } from '../services/user.service';
-import { SnackbarService ,SnackbarType} from '../services/snackbar.service';
+} from "@angular/router";
+import { Observable } from "rxjs";
+import { LoginService } from "../services/login.service";
+import { UserService } from "../services/user.service";
+import { SnackbarService, SnackbarType } from "../services/snackbar.service";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class AuthGuard implements CanActivate {
-  constructor(private router: Router, private lgservice: LoginService,private uDataService:UserService,private sDataService:SnackbarService) {}
+  constructor(
+    private router: Router,
+    private lgservice: LoginService,
+    private uDataService: UserService,
+    private sDataService: SnackbarService
+  ) {}
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
   ): Observable<boolean> | Promise<boolean> | boolean {
     if (this.lgservice.CheckLogged()) {
       return true;
-    }
-    if(this.lgservice.CheckLogged()) {
-      this.router.navigate(['']);
-      this.uDataService.updateUser();
+    } else {
       this.sDataService.open({
-      snackbarType:SnackbarType.error,
-      message:'Dane użytkownika zostały sprawdzone proszę spróbować ponownie.'
-    });
-    }  else {
-      this.lgservice.ChangeLogged();
-      this.sDataService.open({
-        snackbarType:SnackbarType.error,
-        message:'Zostałeś wylogowany!',
+        snackbarType: SnackbarType.error,
+        message: 'Nie jesteś zalogowany!'
       });
+      this.router.navigate(['']);
+      return false;
     }
-    return false;
   }
 }
